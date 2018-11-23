52a2a15badff14bf86502500923672099f38d6dd
Nelly Gabriella <schimko.nelly@gmail.com>
2018-11-23 20:37:51 +0100

    auth layouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper">
+      <div class="row">
+        <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-full-bg">
+          <div class="row w-100">
+            <div class="col-lg-4 mx-auto">
+              <div class="auth-form-dark text-left p-5">
+                <h2>{{ __('Bejelentkezés') }}</h2>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                
+                  <form method="POST" action="{{ route('login') }}" class="pt-5">
+					@csrf
+                    <div class="form-group">
+                      <label for="email">{{ __('E-Mail Cím') }}</label>
+                      <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" id="email" value="{{ old('email') }}" aria-describedby="emailHelp" placeholder="Email" required autofocus>
+					    @if ($errors->has('email'))
+							<span class="invalid-feedback" role="alert">
+								<strong>{{ $errors->first('email') }}</strong>
+							</span>
+						@endif
+                      <i class="mdi mdi-account"></i>
+                    </div>
+                    <div class="form-group">
+                      <label for="password">{{ __('Jelszó') }}</label>
+                      <input type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" name="password" placeholder="Jelszó" required>
+						@if ($errors->has('password'))
+							<span class="invalid-feedback" role="alert">
+								<strong>{{ $errors->first('password') }}</strong>
+							</span>
+						@endif
+                      <i class="mdi mdi-eye"></i>
+                    </div>
+					<div class="form-group">
+						<div class="col-md-6 offset-md-4">
+							<div class="form-check">
+								<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+								<label class="form-check-label" for="remember">
+									{{ __('Jegyezzen meg') }}
+								</label>
+							</div>
+						</div>
+					</div>
+                    <div class="mt-5">
+                      <button type="submit" class="btn btn-block btn-warning btn-lg font-weight-medium">
+                                    {{ __('Bejelentkezés') }}
+                      </button>
+                    </div>
+                    <div class="mt-3 text-center">
+                      @if (Route::has('password.request'))
+						<a class="auth-link text-white" href="{{ route('password.request') }}">
+							{{ __('Elfelejtetted az email címed?') }}
+						</a>
+					  @endif
+                    </div>
+                  </form>                  
+                
+              </div>
             </div>
+          </div>
         </div>
+        <!-- content-wrapper ends -->
+      </div>
+      <!-- row ends -->
     </div>
-</div>
+    <!-- page-body-wrapper ends -->
+  </div>
 @endsection
